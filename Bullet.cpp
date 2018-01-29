d6511dbf2846938960e524583b6e3fa10032b5e9
Silver5478 <bryanboh@gmail.com>
2018-01-29 10:55:55 +0800

    Revert "Revert "Revert "Revert "Worked on player health""""
    This reverts commit feeff0631315a6d93a31422e181a0a5d1abe86e2.

---
+++ Bullet.cpp
@@ -0,0 +1,18 @@
+#include"Bullet.h"
+
+Bullet::Bullet()
+{
+}
+
+void Bullet::draw()
+{
+}
+
+bool Bullet::initialize(Game * gamePtr, int width, int height, int ncols, TextureManager * textureM)
+{
+	return false;
+}
+
+void Bullet::update(float frameTime)
+{
+}
