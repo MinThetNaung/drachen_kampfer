5ded9097023e8cb5f4eebb7ad2631f1048139af8
Bryan Boh <33861831+Silver5478@users.noreply.github.com>
2018-01-29 16:43:45 +0800

    Add files via upload

---
+++ Attack.cpp
@@ -0,0 +1,39 @@
+#include "Attack.h"
+
+
+Attack::Attack() : Entity()
+{
+	collisionType = entityNS::BOX;
+	spriteData.width = AttackNS::WIDTH;        // size of sword
+	spriteData.height = AttackNS::HEIGHT;
+	spriteData.x = AttackNS::X;                   // location on screen
+	spriteData.y = AttackNS::Y;
+	spriteData.rect.bottom = AttackNS::HEIGHT;    // rectangle to select parts of an image
+	spriteData.rect.right = AttackNS::WIDTH;
+	//frameDelay = AttackNS::SWORD_ANIMATION_DELAY;
+	//startFrame = AttackNS::SWORD_START_FRAME;     // first frame of sword animation
+	//endFrame = AttackNS::SWORD_END_FRAME;     // last frame of sword animation
+	currentFrame = startFrame;
+}
+float Attack::setdamage(float d)
+{
+	Damage = d;
+	return Damage;
+}
+void Attack::update(float frameTime)
+{
+	Entity::update(frameTime);
+
+}
+void Attack::damage(WEAPON)
+{
+}
+void Attack::draw()
+{
+	Image::draw();
+}
+bool Attack::initialize(Game *gamePtr, int width, int height, int ncols,
+	TextureManager *textureM)
+{
+	return(Entity::initialize(gamePtr, width, height, ncols, textureM));
+}
