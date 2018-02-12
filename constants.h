8237a025568dd1214192382bca1e4710e8cc626f
Min Thet Naung <s10167248@connect.np.edu.sg>
2018-02-12 23:39:25 +0800

    #updated the background. Still working on camera or maybe GUI

--- constants.h
+++ constants.h
@@ -45,7 +45,7 @@ const float MASS_PLANET = 1.0e14f;
 const float MASS_SHIP = 5.0f;
 const float playerregen = 600;
 // graphic images
-const char BACKGROUND_IMAGE[] =   "pictures\\bg_version1.png";     // photo source NASA/courtesy of nasaimages.org 
+const char BACKGROUND_IMAGE[] =   "pictures\\bg_version2.png";     // photo source NASA/courtesy of nasaimages.org 
 const char TEXTURES_IMAGE[] = "pictures\\textures.png";  // game textures
 const char SHIP_IMAGE[] = "pictures\\ship.png";
 const char BULLET_IMAGE[] = "pictures\\bullet.png";
